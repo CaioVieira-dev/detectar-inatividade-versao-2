2de7fe8efc221cfa03b33b8b28f412ee93252f62
Caio Vieira <caiocesarav0208@gmail.com>
2021-10-15 11:45:04 -0300

    feat: :construction: trabalhar no codigo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect,useRef} from 'react'
+import useTimeout from './hooks/useTimeout'
+const timeout = 5000
+const debounceMaxTime = 200
+
+function IdleVerifier(){
+  const debounceTimeoutId = useRef(null) 
+
+  const {reset} = useTimeout(()=>{
+    // console.log('inativo')
+    alert('inativo')
+  },timeout)
+ 
+  useEffect(()=>{
+    function debounce(){ 
+      // console.log('debounce')
+      if(!debounceTimeoutId.current){
+        debounceTimeoutId.current = setTimeout(()=>{
+          reset()
+          debounceTimeoutId.current = null
+        },debounceMaxTime)
+      }else{ 
+        clearTimeout(debounceTimeoutId.current);
+        debounceTimeoutId.current = setTimeout(()=>{
+          reset()
+          debounceTimeoutId.current = null
+        },debounceMaxTime)
+      }
+    }
+
+    document.addEventListener('keydown',reset,false)
+    document.addEventListener('mousemove',debounce,false)
+    document.addEventListener('touchstart',reset,false);
+    document.addEventListener('scroll',debounce,false)
+    document.addEventListener('touchmove',debounce,false);
+
+    return ()=>{
+      document.removeEventListener('keydown',reset)
+      document.removeEventListener('mousemove',debounce)
+      document.removeEventListener('touchstart',reset)
+      document.removeEventListener('scroll',debounce)
+      document.removeEventListener('touchmove',debounce)
+    }
+  },[reset])
+  return null
+}
 
 function App() {
+  const [isVerifying,setIsVerifying] = useState(true)
+  useEffect(()=>{
+    function handleVisibilityChange() {
+      if(document.visibilityState==='visible'){
+        setIsVerifying(true);
+      }else{ 
+        setIsVerifying(false);
+      }
+    }
+    document.addEventListener('visibilitychange',handleVisibilityChange,false)
+    return ()=>{
+      document.removeEventListener('visibilitychange', handleVisibilityChange)
+    }
+  },[setIsVerifying])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isVerifying && <IdleVerifier />}
       </header>
     </div>
   );
