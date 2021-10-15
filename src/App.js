024e20b82416fbf522722761e6176e7afaa39fb3
Caio Vieira <caiocesarav0208@gmail.com>
2021-10-15 16:45:35 -0300

    chore: :fire: remove comentarios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {//useState,
+import {
   useEffect,
   useRef} from 'react'
 import useTimeout from './hooks/useTimeout'
@@ -7,64 +7,9 @@ import {useLocalStorage} from './hooks/useStorage'
 const timeout = 5000
 const debounceMaxTime = 200
 
-// function IdleVerifier(){
-//   const debounceTimeoutId = useRef(null) 
-
-//   const {reset} = useTimeout(()=>{
-//     // console.log('inativo')
-//     alert('inativo')
-//   },timeout)
- 
-//   useEffect(()=>{
-//     function debounce(){ 
-//       // console.log('debounce')
-//       if(!debounceTimeoutId.current){
-//         debounceTimeoutId.current = setTimeout(()=>{
-//           reset()
-//           debounceTimeoutId.current = null
-//         },debounceMaxTime)
-//       }else{ 
-//         clearTimeout(debounceTimeoutId.current);
-//         debounceTimeoutId.current = setTimeout(()=>{
-//           reset()
-//           debounceTimeoutId.current = null
-//         },debounceMaxTime)
-//       }
-//     }
-
-//     document.addEventListener('keydown',reset,false)
-//     document.addEventListener('mousemove',debounce,false)
-//     document.addEventListener('touchstart',reset,false);
-//     document.addEventListener('scroll',debounce,false)
-//     document.addEventListener('touchmove',debounce,false);
-
-//     return ()=>{
-//       document.removeEventListener('keydown',reset)
-//       document.removeEventListener('mousemove',debounce)
-//       document.removeEventListener('touchstart',reset)
-//       document.removeEventListener('scroll',debounce)
-//       document.removeEventListener('touchmove',debounce)
-//     }
-//   },[reset])
-//   return null
-// }
 
 function App() {
-  // const [isVerifying,setIsVerifying] = useState(true)
-  // useEffect(()=>{
-  //   function handleVisibilityChange() {
-  //     if(document.visibilityState==='visible'){
-  //       setIsVerifying(true);
-  //     }else{ 
-  //       setIsVerifying(false);
-  //     }
-  //   }
-  //   document.addEventListener('visibilitychange',handleVisibilityChange,false)
-  //   return ()=>{
-  //     document.removeEventListener('visibilitychange', handleVisibilityChange)
-  //   }
-  // },[setIsVerifying])
-  //{isVerifying && <IdleVerifier />}
+ 
   const [storageTimeout,
     setStorageTimeout,
     removeStorageTimeout]= useLocalStorage('__example_timeout:',{lastAction: new Date().getTime()+timeout})
